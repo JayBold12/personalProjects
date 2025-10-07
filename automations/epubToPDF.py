ebb78c5e1812782ed757b57854e7ad0149253e4e
JayBold12 <99774394+JayBold12@users.noreply.github.com>
2025-10-07 16:03:30 -0400

    Update epubToPDF.py--path to file/file
    hard coding the path of the input file--fixed with input from user

--- automations/epubToPDF.py
+++ automations/epubToPDF.py
@@ -1,11 +1,13 @@
 """
 1. turn a epub file into a pdf
 
+IMPROVEMENTS
 hard coding the path of the input file
 hard coding the path of the output file
 title of the output file 
 loading for the output
-one file vs multiple"""
+one file vs multiple
+check file path"""
 
 import aspose.pdf as ap
 
@@ -13,8 +15,8 @@ import aspose.pdf as ap
 load_options = ap.EpubLoadOptions()
 
 # Load the EPUB document into a Document object
-document = ap.Document(r"path to epub file here", load_options)
-
+doc_path = input("Paste the path of the epub file here: ")
+document = ap.Document(rf"{doc_path}", load_options)
 # Save the output as a PDF file
 document.save("output.pdf")
 print("EPUB converted to PDF successfully.")
